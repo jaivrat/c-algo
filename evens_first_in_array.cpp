a11681ef9725a9123cb43529e9253df23dd0d180
jaivrat <jaivrat.singh@gmail.com>
2019-12-16 07:48:07 +0800

    adding under dev

---
+++ evens_first_in_array.cpp
@@ -0,0 +1,22 @@
+//
+//  evens_first_in_array.cpp
+//  
+//  Given input array, reorder to have evens first.
+//  Created by Jai Vrat Singh on 16/12/19.
+//
+
+#include <stdio.h>
+
+void reorder(int* A, unsigned int size)
+{
+    int invariant_i = -1;
+    for(j = size -1; j > i; --j)
+    {
+        
+    }
+}
+
+int main()
+{
+    
+}
